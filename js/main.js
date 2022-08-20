b9ce0a76139606eb3754806249a59676c66552de
Sanjar04 <sanjarpolatov200204@gmail.com>
2022-08-19 21:17:28 -0900

    function

---
+++ js/main.js
@@ -0,0 +1,135 @@
+"use strict";
+
+// const result=document.querySelector('#result');
+// const num1=document.getElementById('num1');
+// const num2=document.getElementById('num2');
+
+// const add=() =>{
+//    return result.textContent=num1.value*1+num2.value*1;
+// }
+
+// const minus=() =>{
+//    return result.textContent=num1.value*1-num2.value*1;
+// }
+
+// const multiple=() =>{
+//    return result.textContent=num1.value*1*num2.value*1;
+// }
+
+// const devide=() =>{
+//    return result.textContent=num1.value*1/num2.value*1;
+// }
+
+
+//    function del(){
+//       return  result.textContent = " ",
+//       num1.value = " ",
+//       num2.value = " "
+
+//    }
+
+//====== 1-masala ======//
+
+//   let x = +prompt(" Sonini kiriting: ");
+
+//      function karra(x){
+//         return Math.pow(x,2);
+
+//  }
+//    console.log(karra(x))
+//      karra(x)
+
+//====== 2-masala ======//
+
+//    let num1 = +prompt(" Son1 kiriting: ");
+//    let num2 = +prompt(" Son2 kiriting: ");
+
+//       function plus(num1,num2){
+//          return num1 + num2
+// }
+//    console.log(plus(num1,num2));
+
+//====== 3-masala ======//
+
+//    let num1 = +prompt(" soni1 kiriting: ");
+//    let num2 = +prompt(" soni2 kiriting: ");
+
+//       function minus(num1,num2){
+//          return num1 - num2
+// }
+//    console.log(minus(num1,num2));
+
+//====== 4-masala ======//
+
+   // let x =+prompt("Son1 kiriting: ");
+
+      //=== 2-darajasi ===//
+
+//       function karra(x){
+//          return Math.pow(x,2);
+
+//  }
+//    console.log(karra(x))
+//       karra(x)
+
+      //=== 3-darajasi ===//
+
+//       function karra(x){
+//          return Math.pow(x,3)
+// }
+//    console.log(karra(x))
+//       karra(x)
+
+      //=== 4-darajasi ===//
+
+//       function karra(x){
+//          return Math.pow(x,4)
+// }
+//    console.log(karra(x))
+//       karra(x)
+
+//====== 5-masala ======//
+
+// let n = +prompt(" soni kiriting");
+
+// function toq( n ){
+//    for (let i = 1; i <= n; i++){
+//       i% 2 == 1 ? console.log(i) : console.log();
+//    }
+// }
+//    toq (n)
+
+//====== 6-masala ======//
+
+// let n = +prompt(" soni kiriting");
+
+//  function juft( n ){
+//     for (let i = 1; i <= n; i++){
+//        i% 2 == 0 ? console.log(i) : console.log();
+//     }
+// }
+//     juft (n)
+
+//====== 7-masala ======//
+
+//    let n = +prompt(" Son kiriting: ");
+
+//       function increment (incrementNumber , decrementNumber){
+//          let inc = ++incrementNumber;
+//          let dec = -- decrementNumber;
+
+//       return ` ${inc} , ${dec}`
+//  }
+
+//    console.log(increment(n,n))
+
+//====== 8-masala ======//
+
+//    let n = +prompt(" soni kiriting: ");
+
+//       function son( n ){
+//          for (let i = 1; i <= n; i++){
+//             n% i == 0 ? console.log(i) : console.log()
+//          }
+// }
+//       son( n )
